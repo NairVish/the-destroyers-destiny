65b207a774ff61d40d366f5df56cd72b2e92d23a
Vishnu Nair <vishnu4nair@gmail.com>
2015-10-12 17:10:46 -0400

    Added docstrings to startState.py

--- startState.py
+++ startState.py
@@ -2,68 +2,94 @@ __author__ = 'vishnunair'
 
 import os
 import time
-import player
+import globals
+from exit import exit_program
 
-def print_intro():  # We're printing out our awesome intro screen here.
+def print_intro():
+    """ Print the intro screen. """
     intro_text = (
         "THIS IS SOME INTRO TEXT."
         "I HAVE NO IDEA WHAT I'M GOING TO PUT HERE."
         "SO I'M PUTTING SOME PLACEHOLDER TEXT HERE."
     )
     print(intro_text)
-    time.sleep(5)
-    os.system('cls' if os.name == 'nt' else 'clear')
+    time.sleep(4)
+    globals.clear_screen()
 
-def show_start_menu():  # The main menu
+def show_start_menu():  # The startup menu
+    """
+    Prints the main startup menu and and requests user input of either starting New Game, continuing (if save is found)
+    """
     print('Main Menu')
     print('\t1. Start a New Game.')
-    accepted_answers = ['1','1.']
+    accepted_answers = ['1','q']
     save = find_save()
     if save is not None:
-        print('\t2. Continue from save.')
-        accepted_answers = ['1','2','1.','2.']
-    answer = input('Choose your desired option.')
+        print('\t2. Continue from existing save.')
+        print('\tq. Quit')
+        accepted_answers = ['1','2','q']
+    answer = input('Choose your desired option. ')
     while answer not in accepted_answers:
         answer = input('You have entered an inavlid option. Please enter a valid option,')
-    os.system('cls' if os.name == 'nt' else 'clear')
-    if answer is '1' or '1.':
+    globals.clear_screen()
+    if answer is '1':
         return None
+    elif answer is 'q':
+        exit_program()
     else:
         return save
 
-
 def find_file(name, path):
+    """
+    :param name: Name of file
+    :param path: Directory to look in
+    Looks for a specific file in a specific directory. Returns path if found; otherwise returns None.
+    """
     for root, dirs, files in os.walk(path):
         if name in files:
             return os.path.join(root, name)
 
 def find_save():
+    """
+    Looks for a game save file in the current working directory.
+    If not found, returns None.
+    If found, returns opened file.
+    """
     result = find_file('save.data', os.getcwd())
     if result == None:
         return None
-
-    answer = ''
-    print('A save file has been detected in the game directory.')
-    answer = input('Would you like to load this save? (Y/N)').lower()
-    accepted_answers = ['y','n','yes','no']
-    while answer not in accepted_answers:
-        answer = input('You have entered an inavlid option. Please enter a valid option,').lower()
-    if 'y':
+    else:
         return open('save.data','r')
-    elif 'n':
-        return None
 
 def load_player():
+    """
+    Requests save file from user's input at startup menu.
+    If there is no save file OR user specifically requests new game, asks for character name and declares new player
+    as a global instance.
+    If there is a save file AND user requests continuing from save, extract data and inventory (if there are items)
+    from save and declares new global player instance using existing data.
+    """
     save = show_start_menu()
-    global player
     if save is None:
         print("NEW GAME START")
         name = input('Please enter a name for your character: ')
-        player = player.Player(name)
+        globals.declare_new_player(name)
     else:
-        save_data = [line.rstrip('\n') for line in save.readlines()]
-        inventory = [line for line in save_data[10:(len(save_data)-1)]]
+        saved_stats = [line.rstrip('\n') for line in save.readlines()]
+        try:
+            inventory = [line for line in saved_stats[10:(len(saved_stats)-1)]]
+        except IndexError:
+            inventory = []
+        else:
+            pass
         print("Loading existing save...")
-        print("Loading save data for %s" % save_data[0])
-        player = player.Player(save_data[0],save_data[1],save_data[2],save_data[3],save_data[4],save_data[5],
-                               save_data[6],save_data[7],save_data[8],save_data[9], inventory)
+        print("Loading save data for %s" % saved_stats[0])
+        globals.declare_existing_player(saved_stats, inventory)
+        save.close()
+
+def startSequence():
+    """
+    Executes start sequence for start state
+    """
+    print_intro()
+    load_player()
