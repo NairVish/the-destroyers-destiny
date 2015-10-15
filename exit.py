00cc370dba73010838211c7261cef6a8eaf2f9bc
Vishnu Nair <vishnu4nair@gmail.com>
2015-10-15 10:41:45 -0400

    Wrote save prompt as well as actual save mechanic.

--- exit.py
+++ exit.py
@@ -7,3 +7,38 @@ def exit_program():
     print("The program has ended. Thank you for playing.")
     print('\n')
     sys.exit()
+
+def prompt_for_save():
+    globals.clear_screen()
+    inp = input("Would you like to save the game? (y/n) ")
+    if inp is 'y':
+        save_game()
+    else:
+        return
+
+def save_game():
+    with open("save,data",'w') as save:
+        save.truncate()
+        player = globals.this_player
+        save.writelines(player.name + '\n')
+        save.writelines(player.province + '\n')
+        save.writelines(player.level + '\n')
+        save.writelines(player.xp + '\n')
+        save.writelines(player.target_xp + '\n')
+        save.writelines(player.health + '\n')
+        save.writelines(player.total_health + '\n')
+        save.writelines(player.attack + '\n')
+        save.writelines(player.defense + '\n')
+        save.writelines(player.speed + '\n')
+        save.writelines(player.main_quest_stage + '\n')
+        save.writelines(player.money + '\n')
+        save.writelines(player.assistant + '\n')
+        save.writelines(player.current_weapon + '\n')
+        save.writelines(player.day + '\n')
+        save.writelines(player.sidequests + '\n')
+        for item in player.inventory:
+            save.writelines(item + '\n')
+
+def exit_sequence():
+    prompt_for_save()
+    exit_program()
