dd93474647638d3bf6b3db3cf758465df17cdb3b
Vishnu Nair <vishnu4nair@gmail.com>
2015-10-28 10:03:37 -0400

    Optimized imports and added docstrings to all functions in battle.py.

--- battle.py
+++ battle.py
@@ -1,9 +1,18 @@
 __author__ = 'vishnunair'
-import globals
 from random import randrange
+import globals
 
-class Enemy():
+
+class Enemy:
+    """
+    The Enemy class holds all the data for the enemy in a battle.
+    """
     def __init__(self, enemy_name, enemy_type):
+        """
+        :param enemy_name: The enemy's name.
+        :param enemy_type: The enemy type (bandit, looter, mobster, valstr).
+        Initializes the enemy and its attributes.
+        """
         self.enemy_name = enemy_name
         self.enemy_type = enemy_type
         self.level = self.determine_enemy_level()
@@ -13,25 +22,47 @@ class Enemy():
         self.max_health = self.health
 
     def determine_enemy_level(self):
+        """
+        Determines and returns the enemy's level. Regular enemies are half a level below the player, while boss levels
+        equal that of the player.
+        """
         if self.enemy_type is "reg":
             return globals.this_player.level-0.5
         elif self.enemy_type is "boss":
             return globals.this_player.level
 
     def determine_health(self):
+        """
+        Determines and returns the enemy's health. Regular enemies have half the player's health; bosses
+        have 95% of the player's health.
+        """
         if self.enemy_type is "reg":
             return int(globals.this_player.total_health*0.5)
         else:
             return int(globals.this_player.total_health*0.95)
 
-class Battle():
-    def __init__(self, enemy_name, type="reg"):   # regular ("reg") or boss ("boss") enemy
+
+class Battle:
+    """
+    The Battle class holds all data for and executes the battle mechanic.
+    """
+    def __init__(self, enemy_name, type="reg"):
+        """
+        :param enemy_name: The name of the enemy (for the enemy's __init__ function).
+        :param type: The type of the enemy ((for the enemy's __init__ function).
+        Initialized a battle with an enemy. Also, determines possible XP gained from the battle as well
+        as the player's max health.
+        """
         self.enemy = Enemy(enemy_name, type)
         self.possible_xp = int(self.enemy.level) + 3
         # NOTE: player health is globals.this_player.current_health
         self.p_max_health = globals.this_player.total_health
 
     def do_battle(self):
+        """
+        Executes the battle sequence until either the player or the enemy runs out of health. If the player runs
+        out of health, the GameOver exception is raised. Else, the battle is ended, and XP is awarded.
+        """
         while globals.this_player.current_health > 0 and self.enemy.health > 0:
             self.show_status()
             self.execute_move()
@@ -51,11 +82,19 @@ class Battle():
             return
 
     def show_status(self):
+        """
+        Prints the battle's current status.
+        """
         print("BATTLE WITH %s" % self.enemy.enemy_name.upper())
         print("Your health: %s/%s" % (globals.this_player.current_health, self.p_max_health))
         print("Enemy health: %s/%s" % (self.enemy.health, self.enemy.max_health))
 
     def execute_move(self):
+        """
+        Determines damage cast by the player to the enemy using a (frankly overly complex) formula and deducts from
+        the enemy's health and vice versa. If the the player's assistant flag is set to True, there is a small (10%)
+        chance that the assistant will attack too. If the skip flag is True, the player's turn is skipped.
+        """
         skip = self.show_menu()
         globals.clear_screen()
         if skip is True:
@@ -81,6 +120,10 @@ class Battle():
 
 
     def show_menu(self):
+        """
+        Accepts and executes the player's desired action on the main battle screen. Returns True if the player uses a potion
+        (from their inventory). Returns False in all other situations.
+        """
         self.show_options()
         inp = input("Choose an option: ")
 
@@ -109,6 +152,9 @@ class Battle():
         return False
 
     def show_options(self):
+        """
+        Prints the battle screen menu.
+        """
         print("Battle Menu:\n"
               "\t1. Attack!\n"
               "\t2. Use health item (lose turn).\n"
